c10a96468176c485b96d99593e3cfa6c1e46e9ac
Sam Leone <sam.leone@yale.edu>
2023-04-23 23:35:28 -0400

    Parameter estimation

--- spectral_denoiser.py
+++ spectral_denoiser.py
@@ -5,6 +5,10 @@ import torch
 from torch import optim
 from tqdm import tqdm
 import warnings
+import cvxopt as opt
+from cvxopt import matrix, spmatrix, sparse
+from cvxopt.solvers import qp, options
+from cvxopt import blas
 
 class Spectral_Denoiser:
     # Initialize
@@ -14,14 +18,75 @@ class Spectral_Denoiser:
             self.G = Graph
         else:
             raise ValueError("Graph Required")
+            
+    def bandlimit_low(self, f_tilde, omega=-1, k=-1):
+        # Remove Gaussian Noise
+        # Input: f_tilde, the observed signal
+        # tau: eta * sigma^2 is a smoothing parameter
+        # output: MLE original signal
+
+        if omega > -1:
+            f_tilde = f_tilde.reshape(-1)
+            gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1*(x <= omega))
+            f_check = gauss_filter.filter(f_tilde ,method = 'chebyshev')
+            return f_check
+        elif k > -1:
+            G = self.G
+            G.compute_fourier_basis()
+            self.Psi = G.U
+            self.evals = G.e
+            omega = self.evals[k]
+            gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1*(x <= omega))
+            f_check = gauss_filter.filter(f_tilde ,method = 'exact')
+            return f_check
+        else:
+            raise ValueError("Can Only Filter Based on Bandwidth")
+            
+        f_tilde = f_tilde.reshape(-1)
+        gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1/(1 + tau * x))
+        f_check = gauss_filter.filter(f_tilde ,method = 'chebyshev')
+        return f_check
     
-    def remove_gaussian_noise(self, f_tilde, tau = 1):
+    def bandlimit_high(self, f_tilde, omega=-1, k=-1):
+        # Remove Gaussian Noise
+        # Input: f_tilde, the observed signal
+        # tau: eta * sigma^2 is a smoothing parameter
+        # output: MLE original signal
+
+        if omega > -1:
+            f_tilde = f_tilde.reshape(-1)
+            gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1*(x >= omega))
+            f_check = gauss_filter.filter(f_tilde ,method = 'chebyshev')
+            return f_check
+        elif k > -1:
+            G = self.G
+            G.compute_fourier_basis()
+            self.Psi = G.U
+            self.evals = G.e
+            omega = self.evals[-k]
+            gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1*(x >= omega))
+            f_check = gauss_filter.filter(f_tilde ,method = 'exact')
+            return f_check
+        else:
+            raise ValueError("Can Only Filter Based on Bandwidth")
+            
+        f_tilde = f_tilde.reshape(-1)
+        gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1/(1 + tau * x))
+        f_check = gauss_filter.filter(f_tilde ,method = 'chebyshev')
+        return f_check
+    
+    def remove_gaussian_noise(self, f_tilde, tau = None):
         # Remove Gaussian Noise
         # Input: f_tilde, the observed signal
         # tau: eta * sigma^2 is a smoothing parameter
         # output: MLE original signal
         
-        f_tilde = f_tilde.reshape(-1)
+        if tau == None:
+            tau = self.estimate_tau_for_gaussian(f_tilde)
+        
+        if tau < 0:
+            tau = 1
+        
         gauss_filter = pygsp.filters.Filter(self.G, lambda x : 1/(1 + tau * x))
         f_check = gauss_filter.filter(f_tilde ,method = 'chebyshev')
         return f_check
@@ -33,66 +98,184 @@ class Spectral_Denoiser:
         
         # Output: f_check - the MLE estimate / Solution to the Heat Equation
         
-        f_tild_vec = f_tilde.reshape(-1)
-        n = self.G.N
-        
-        # extrapolate where f_tilde is zero and nonzero
-        I = np.where(f_tild_vec != 0)
-        Ic = np.where(f_tild_vec == 0)
-        sz_I = len(I[0])
-        sz_Ic = len(Ic[0])
-        
-        # get Adjacency matrix
-        
-        Adj = self.G.A.todense()*1
-        
-        if method == 'exact':
-            if sz_I > 10000:
-                warnings.warn("Approximation is recommended for large problems")
+        restored = np.zeros(f_tilde.shape)
+        for signal_index in range(f_tilde.shape[1]):
+            f_tild_vec = f_tilde[:,signal_index].reshape(-1)
+            n = self.G.N
+
+            # extrapolate where f_tilde is zero and nonzero
+            I = np.where(f_tild_vec != 0)
+            Ic = np.where(f_tild_vec == 0)
+            sz_I = len(I[0])
+            sz_Ic = len(Ic[0])
+
+            # get Adjacency matrix
+
+            Adj = self.G.A.todense()*1
+
+            if method == 'exact':
+                if sz_I > 10000:
+                    warnings.warn("Approximation is recommended for large problems")
+
+                # create relevant matrices
+                Lap = self.G.L.todense()
+
+                subLap = Lap[Ic][:,Ic].reshape(sz_Ic,sz_Ic)
+                subAdj = Adj[Ic][:,I].reshape(sz_Ic, sz_I)
+                f_tild_I = f_tild_vec[I]
+
+                # diffuse over the boundary and find the inverse
+                delta = np.linalg.solve(subLap,(subAdj@f_tild_I).reshape(-1,1))
+                delta = np.array(delta).reshape(-1)
+                f_check = f_tild_vec.copy()
+
+                # Impute corresponding Indices
+                for i in range(sz_Ic):
+                    f_check[Ic[0][i]] = delta[i]
+
+            elif method == 'approximate':
+                x = f_tild_vec.copy()
+                # initialize walk matrix
+                P = np.zeros((n,n))
+                for i in range(n):
+                    if f_tild_vec[i] != 0:
+                        # Stabilize at the known vertices
+                        P[i,i] = 1
+                    else:
+                        # Diffuse Otherwise
+                        P[i,:] = Adj[i,:]/np.sum(Adj[i,:])
+
+                # iterate
+                for iter in tqdm(range(time)):
+                    x_prev = x.copy()
+                    x = P@x
+                    if np.linalg.norm(x_prev-x) < 1e-4:
+                        break
+
+                f_check = x
+            else:
+                raise Exception("Methods are 'approximate' or 'exact'")
             
-            # create relevant matrices
-            Lap = self.G.L.todense()
+            restored[:,signal_index] = f_check.reshape(-1)
+        
+        return restored
+    
+    def estimate_tau_for_gaussian(self, tilde_samples):
+        Lap = self.G.L.todense()
+        n = Lap.shape[0]
+        num_samps = tilde_samples.shape[1]
+        
+        order_1 = 0
+        order_2 = 0
+        
+        for i in range(num_samps):
+            theta_tilde = tilde_samples[:,i]
+            order_1 += theta_tilde.T@Lap@theta_tilde
+            order_2 += np.linalg.norm(Lap@theta_tilde)**2
+        
+        order_1 = order_1/num_samps; order_2 = order_2/num_samps
 
-            subLap = Lap[Ic][:,Ic].reshape(sz_Ic,sz_Ic)
-            subAdj = Adj[Ic][:,I].reshape(sz_Ic, sz_I)
-            f_tild_I = f_tild_vec[I]
+        numerator = (n-1)*order_2 - np.trace(Lap)*order_1
+        denominator = (np.linalg.norm(Lap)**2)*order_1 - np.trace(Lap)*order_2
+        return numerator/denominator
+    
+    def estimate_eta_for_uniform(self, tilde_samples, Linv = None):
+        Lap = self.G.L.todense()
+        n = Lap.shape[0]
+        num_samps = tilde_samples.shape[1]
+        
+        if Linv is None:
+            Linv = np.linalg.pinv(Lap)
 
-            # diffuse over the boundary and find the inverse
-            delta = np.linalg.solve(subLap,(subAdj@f_tild_I).reshape(-1,1))
-            delta = np.array(delta).reshape(-1)
-            f_check = f_tild_vec.copy()
+        avg_t = 0
+        for i in range(num_samps):
+            avg_t += np.linalg.norm(tilde_samples[:,i])**2
 
-            # Impute corresponding Indices
-            for i in range(sz_Ic):
-                f_check[Ic[0][i]] = delta[i]
-                
-        elif method == 'approximate':
-            x = f_tild_vec.copy()
-            # initialize walk matrix
-            P = np.zeros((n,n))
+        avg_t = avg_t/num_samps
+
+        mu_sq = (2*np.sqrt(n)*np.mean(tilde_samples))**2
+        # print(mu_sq)
+
+        numerator = np.trace(Linv)
+        denominator = 6*avg_t - 2*mu_sq
+
+        return numerator/denominator
+    
+    def remove_uniform_noise(self, theta_tilde, max_iter = 10, true_signal = None, max_iter_qp=20, epsilon = 1e-3, bump=1, eta=None):
+        
+        Lap = self.G.L.todense()
+        n = Lap.shape[0]
+
+        # perform method of moments estimation for eta
+
+        if eta == None:
+            eta = self.estimate_eta_for_uniform(theta_tilde)
+            print(eta)
+        
+        def convex_subroutine(L_sub, theta_tilde_sub, x_k):
+            options['show_progress'] = False
+            options["maxiters"] = max_iter_qp
+
+            theta_tilde_sub = theta_tilde_sub.reshape(-1)
+
+            n = max(theta_tilde_sub.shape)
+
+            Uniform_Enforcer_Matrix = np.zeros((n,n))
+            Uniform_Enforcer_Vector = -np.ones((n,1)).reshape(-1,1)
+
             for i in range(n):
-                if f_tild_vec[i] != 0:
-                    # Stabilize at the known vertices
-                    P[i,i] = 1
-                else:
-                    # Diffuse Otherwise
-                    P[i,:] = Adj[i,:]/np.sum(Adj[i,:])
+                Uniform_Enforcer_Matrix[i,i] = - 1/theta_tilde_sub[i]
+               
+
+            concave_gradient = (1/np.abs(x_k)).reshape(-1,1)
+            x_k_plus_1 = qp(matrix(L_sub), matrix(concave_gradient), matrix(Uniform_Enforcer_Matrix), matrix(Uniform_Enforcer_Vector))['x']
             
-            # iterate
-            for iter in tqdm(range(time)):
-                x_prev = x.copy()
-                x = P@x
-                if np.linalg.norm(x_prev-x) < 1e-4:
-                    break
-                    
-            f_check = x
+            return np.array(x_k_plus_1).reshape(-1,1)
+        
+        def primal_cost(x):
+            x = x.reshape(-1,1)
+            return eta*x.T@Lap@x + np.sum(np.log(np.abs(x)))
+        
+        if true_signal is None:
+            print("Truth unknown")
         else:
-            raise Exception("Methods are 'approximate' or 'exact'")
+            true_cost = primal_cost(true_signal)
+            print(f'True cost: {true_cost}')
+        
+        # Optimize for every signal 
+
+        restored_signals = np.zeros((theta_tilde.shape[0],theta_tilde.shape[1]))
+        # print(theta_tilde.shape[1])
+        for signal_index in np.arange(theta_tilde.shape[1]):
             
-        return f_check
+            noisy_signal = theta_tilde[:,signal_index].reshape(-1,1)
+
+            x0 = 1.05*noisy_signal
+
+            prev_cost = primal_cost(x0)
+            relative_stopping_criteria = primal_cost(x0)*epsilon
+            scaled_bump = bump*np.max(x0)
+            x_k = x0 + np.ones((n,1))*scaled_bump*np.sign(x0)
+            losses = []
+            
+            i = 0
+            delta = relative_stopping_criteria + 1
+            new_cost = prev_cost + relative_stopping_criteria + 1
+            
+            while (i < max_iter) and delta > relative_stopping_criteria:
+                prev_cost = new_cost
+                losses.append(primal_cost(x_k))
+                x_k = convex_subroutine(eta*Lap, noisy_signal, x_k)
+                new_cost = primal_cost(x_k)
+                delta = prev_cost - new_cost
+                i += 1
+            
+            print(f"Final Cost: {new_cost} Achieved in {i} Iterations")
+            restored_signals[:,signal_index] = x_k.reshape(-1)
+            
+        return restored_signals
     
-    
-    def remove_uniform_noise(self, f_tilde, lr = 1, alpha = 1, beta = 1, gamma = 1, MAX_ITER = 500):
+    def remove_uniform_noise_old(self, f_tilde, lr = 1, alpha = 1, beta = 1, gamma = 1, MAX_ITER = 500):
         
         #Removes Uniformly Distributed Noise
         # Inputs: 
